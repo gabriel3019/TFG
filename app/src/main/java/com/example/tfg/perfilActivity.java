86bf683433da6f0ebf6b43cda3c679c2024f8854
Gabriel <gabrielbr190@gmail.com>
2025-04-04 11:15:04 +0200

    btnMenu creado

--- app/src/main/java/com/example/tfg/perfilActivity.java
+++ app/src/main/java/com/example/tfg/perfilActivity.java
@@ -1,6 +1,8 @@
 package com.example.tfg;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,6 +15,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class perfilActivity extends AppCompatActivity {
 
     private EditText etNombre, etApellidos, etCorreo;
+    private Button btnMenu;
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
 
@@ -29,6 +32,15 @@ public class perfilActivity extends AppCompatActivity {
         etNombre = findViewById(R.id.nombre);
         etApellidos = findViewById(R.id.apellidos);
         etCorreo = findViewById(R.id.correo_electronico);
+        btnMenu = findViewById(R.id.btnMenu);
+
+        // Establecer el OnClickListener para el botón
+        btnMenu.setOnClickListener(v -> {
+            // Crear un Intent para ir a la actividad del menú
+            Intent intent = new Intent(perfilActivity.this, menuActivity.class); // Asegúrate de que el nombre de la actividad sea correcto
+            startActivity(intent); // Iniciar la actividad
+        });
+
 
         cargarDatosUsuario();
     }
