8a5b3e910d24a219d1ec71a8a90e3f795a7fa9df
gabriel3019 <celianmolin@gmail.com>
2025-04-08 15:14:22 +0200

    cartaActivity Hecho

--- app/src/main/java/com/example/tfg/menuActivity.java
+++ app/src/main/java/com/example/tfg/menuActivity.java
@@ -2,6 +2,7 @@ package com.example.tfg;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,16 +11,27 @@ public class menuActivity extends AppCompatActivity {
 
     private ImageButton btnPerfil;
 
+    private Button btnCarta;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
         btnPerfil = findViewById(R.id.Perfil);
+        btnCarta = findViewById(R.id.btnCarta);
+
 
         btnPerfil.setOnClickListener(v -> {
             Intent intent = new Intent(menuActivity.this, perfilActivity.class);
             startActivity(intent);
         });
+
+        btnCarta.setOnClickListener(v -> {
+            Intent intent = new Intent(menuActivity.this, cartaActivity.class);
+            startActivity(intent);
+        });
     }
+
+
 }
