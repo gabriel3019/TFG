da4534a8678466b95322da13c40095e054c7f2fa
gabriel3019 <celianmolin@gmail.com>
2025-04-10 19:32:53 +0200

    cartaTerminada

--- app/src/main/java/com/example/tfg/cartaActivity.java
+++ app/src/main/java/com/example/tfg/cartaActivity.java
@@ -1,6 +1,10 @@
 package com.example.tfg;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class cartaActivity extends AppCompatActivity {
 
+    private Button btnMenu;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_carta);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+       btnMenu = findViewById(R.id.btnMenu);
+
+        // Establecer el OnClickListener para el botón
+        btnMenu.setOnClickListener(v -> {
+            // Crear un Intent para ir a la actividad del menú
+            Intent intent = new Intent(cartaActivity.this, menuActivity.class); // Asegúrate de que el nombre de la actividad sea correcto
+            startActivity(intent); // Iniciar la actividad
         });
+
     }
+
 }
